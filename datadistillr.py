7abd021a44778645eb208856b4b78c3b31b29725
ForrestRogers-datadistillr <86311094+ForrestRogers-datadistillr@users.noreply.github.com>
2021-06-24 11:06:40 -0700

    Update datadistillr.py

--- datadistillr.py
+++ datadistillr.py
@@ -1,14 +1,34 @@
+import requests
+import http.cookiejar
+import json
+jar = http.cookiejar.CookieJar()
+
 class datadistillr:
-    def datadistillr():
-            import requests
 
-            login = "https://devapp.datadistillr.io/api/login"
-            logininfo = {"email":"forrest@datadistillr.com","password":"abcd1234"}
-            logout = "https://devapp.datadistillr.io/api/logout"
-            projects = "https://devapp.datadistillr.io/api/projects"
+    def __init__(self):
+        self.session = requests.session()
 
-            requests.post(url = login,params = logininfo)
-            requests.get(url = logout)
-            requests.get(url = projects)
+        self.login_page = "https://devapp.datadistillr.io/api/login"
+        self.logout_page = "https://devapp.datadistillr.io/api/logout"
+        self.projects_page = "https://devapp.datadistillr.io/api/projects"
+
+
+    def login(self, email, password):
+        user_info = {
+            "email": str(email), 
+            "password": str(password),
+            }
+        
+        json_user_info = json.dumps(user_info)
+        
+        requests.post(url = self.login_page, params = json_user_info, cookies = jar)
+
+    def logout(self):
+        requests.get(url = self.logout_page, cookies = jar)
+
+    def get_projects(self):
+        requests.get(url = self.projects_page, cookies = jar)
+
+
 
 
