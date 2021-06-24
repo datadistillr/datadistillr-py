89f4daf5c70216fdb00621a1bc6a44496b9a0ad8
ForrestRogers-datadistillr <86311094+ForrestRogers-datadistillr@users.noreply.github.com>
2021-06-24 11:12:10 -0700

    Update datadistillr.py

--- datadistillr.py
+++ datadistillr.py
@@ -1,19 +1,25 @@
 import requests
 import http.cookiejar
 import json
-jar = http.cookiejar.CookieJar()
+
 
 class datadistillr:
+    """Cookie Jar and the url pages based off the base url"""
+    jar = http.cookiejar.CookieJar()
+    base_url = "https://devapp.datadistillr.io/api/"
+    login_page = base_url + "login"
+    logout_page = base_url + "logout"
+    projects_page = base_url + "projects"
 
     def __init__(self):
-        self.session = requests.session()
-
-        self.login_page = "https://devapp.datadistillr.io/api/login"
-        self.logout_page = "https://devapp.datadistillr.io/api/logout"
-        self.projects_page = "https://devapp.datadistillr.io/api/projects"
+        #self.login_page = "https://devapp.datadistillr.io/api/login"
+        #self.logout_page = "https://devapp.datadistillr.io/api/logout"
+        #self.projects_page = "https://devapp.datadistillr.io/api/projects"
+        pass
 
 
     def login(self, email, password):
+        """Takes in email and password, converts to dictionary, then converts to JSON, and posts the user info to the login page"""
         user_info = {
             "email": str(email), 
             "password": str(password),
@@ -21,13 +27,18 @@ class datadistillr:
         
         json_user_info = json.dumps(user_info)
         
-        requests.post(url = self.login_page, params = json_user_info, cookies = jar)
+        return requests.post(url = self.login_page, params = json_user_info, cookies = self.jar)
 
     def logout(self):
-        requests.get(url = self.logout_page, cookies = jar)
+        """Gets logout page"""
+        return requests.get(url = self.logout_page, cookies = self.jar)
 
     def get_projects(self):
-        requests.get(url = self.projects_page, cookies = jar)
+        """Gets projects page"""
+        return requests.get(url = self.projects_page, cookies = self.jar)
+
+
+
 
 
 
