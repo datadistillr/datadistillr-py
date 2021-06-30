ff98945683932308460163e07101455df05e8bcc
ForrestRogers-datadistillr <86311094+ForrestRogers-datadistillr@users.noreply.github.com>
2021-06-30 13:28:51 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,32 +1,28 @@
-import setuptools
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name = "Datadistillr",
-    version = "1.0",
-    author = "Forrest Rogers",
-    author_email = "forrest@datadistillr.com",
-    description = "Be able to easily use Datadistillr in python",
-    long_description = long_description,
-    url = "https://github.com/datadistillr/datadistillr-py",
-    packages = setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: Apache License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires = ">=3.6",
-    install_requires=[],
-    keywords = "datadistillr",
-    project_urls={
-        "Homepage" : "https://github.com/datadistillr/datadistillr-py"
-    },
-    entrypoints = {[]},
-
-
-
-
-
+from distutils.core import setup
+setup(
+  name = 'datadistillr-py',         # How you named your package folder (MyLib)
+  packages = ['datadistillr-py'],   # Chose the same as "name"
+  version = '0.1',      # Start with a small number and increase it with every change you make
+  license='apache-2.0,        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'Be able to easily use Datadistillr from Python',   # Give a short description about your library
+  author = 'Forrest Rogers',                   # Type in your name
+  author_email = 'forrest@datadistillr.com',      # Type in your E-Mail
+  url = 'https://github.com/datadistillr/datadistillr-py',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/datadistillr/datadistillr-py/archive/refs/tags/v_01.tar.gz',    # I explain this later on
+  keywords = ['Datadistillr', 'Database', 'Data'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'requests',
+          'http.cookiejar',
+          'json',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: Apache License 2.0  ',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
 )
