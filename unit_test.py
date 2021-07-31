18bd67aefe1c45861282af44c9bd27aa5e609768
ForrestRogers-datadistillr <86311094+ForrestRogers-datadistillr@users.noreply.github.com>
2021-07-31 02:00:29 -0700

    Update unit_test.py

--- unit_test.py
+++ unit_test.py
@@ -1,1 +1,25 @@
+import unittest
+import datadistillr
 
+class TestDataDistillrMethods(unittest.TestCase):
+    
+    def test_login(self):
+        self.assertEqual(datadistillr.login(), "", msg= "Login successful") 
+
+    def test_logout(self):
+        self.assertEqual(datadistillr.logout(), "", msg= "Logout successful")
+
+    def test_get_projects(self):
+        self.assertEqual(datadistillr.get_projects, "", msg= "Getting projects successful")
+
+    def test_project_details(self):
+        self.assertEqual(datadistillr.project_details, "")
+
+    def test_queries(self):
+        self.assertEqual(datadistillr.queries(), "")
+
+    def test_query_results(self):
+        self.assertEqual(datadistillr.query_results(), "")
+
+    def test_create_project(self):
+        self.assertEqual(datadistillr.create_project(), "")
