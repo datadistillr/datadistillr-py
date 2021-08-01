ab586bc6d62bf100159f8b89e73af8577e46ddae
ForrestRogers-datadistillr <86311094+ForrestRogers-datadistillr@users.noreply.github.com>
2021-07-31 20:48:37 -0700

    Update unit_test.py

--- unit_test.py
+++ unit_test.py
@@ -5,10 +5,11 @@ import json
 class TestDataDistillrMethods(unittest.TestCase):
     def __init__(self, methodName: str) -> None:
         super().__init__(methodName=methodName)
+        pass
 
-        self.loginname = "Name"
-        self.loginemail = "Email"
-        self.loginresponse: dict({
+    
+    def test_login(self):
+        loginresponse = {
             "env":"production",
             "buildNumber":"1626204389575",
             "messages":None,
@@ -28,26 +29,26 @@ class TestDataDistillrMethods(unittest.TestCase):
             "hasOrganizationInvitation":False,
             "userSetupOrganization":True,
             "user":{
-            "email":self.loginemail,
+            "email":"",
             "last_activity":{
-            "date":"2021-07-31 20:19:56.000000",
+            "date":"",
             "timezone_type":3,
-            "timezone":"America/Chicago"},
-            "last_ip_address":"192.145.118.90",
+            "timezone":""},
+            "last_ip_address":"",
             "last_login":{
-            "date":"2021-07-31 20:19:56.000000",
+            "date":"",
             "timezone_type":3,
-            "timezone":"America/Chicago"},
+            "timezone":""},
             "organizations":[{
             "name":"hidden",
             "level":"personal",
             "domain":None,
             "logo":None,
             "role":"admin",
-            "createdBy":self.loginname,
+            "createdBy":"",
             "token":1135197221,
             "created_by_token":1393626227}],
-            "vars":{"name":self.loginname},
+            "vars":{"name":""},
             "token":1393626227,
             "defaultOrgToken":1135197221,
             "active":None,
@@ -55,15 +56,13 @@ class TestDataDistillrMethods(unittest.TestCase):
             "activityString":"Active Just now",
             "gravatarUrl":"https://www.gravatar.com/avatar/cd1649a417cafdef00d4b85674c0eb97?d=https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/pilot_traveller_person_avatar-512.png"},
             "webSocketUrl":"wss://jw5kcbx4sd.execute-api.us-east-1.amazonaws.com/production",
-            "notificationCount":0})
-
-    
-    def test_login(self):
-        self.assertEqual(datadistillr.login(), self.loginresponse, msg= "Login successful") 
-        self.assertNotEqual(datadistillr.login(), self.loginresponse, msg= "Login failed") 
+            "notificationCount":0}
+        self.assertEqual(datadistillr.login(), loginresponse, msg= "Login successful") 
+        self.assertNotEqual(datadistillr.login(), loginresponse, msg= "Login failed") 
 
     def test_logout(self):
-        self.assertEqual(datadistillr.logout(), "", msg= "Logout successful")
+        logoutreponse = {"env":"production","buildNumber":"1626204389575","messages":None,"loggedIn":False,"message":"You are logged out.","session_destroyed":True}
+        self.assertEqual(datadistillr.logout(), logoutreponse, msg= "Logout successful")
 
     def test_get_projects(self):
         self.assertEqual(datadistillr.get_projects, "", msg= "Getting projects successful")
